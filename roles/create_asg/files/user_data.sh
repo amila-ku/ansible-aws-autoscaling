4a24a178365fc27d755c1bbe642d1dc60ca7fa93
Amila Kumaranayaka <amila.kumaranayaka@cloudpac.io>
2017-09-23 16:45:33 +0530

    added userdata with docker,git install

---
+++ roles/create_asg/files/user_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install -y docker git
+sudo usermod -a -G docker ec2-user
+sudo service docker start
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
