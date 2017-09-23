7408a10376cc3c6829be555ad1d5c7c0de54d2fd
Amila Kumaranayaka <amila.kumaranayaka@cloudpac.io>
2017-09-23 22:13:54 +0530

    added save aws private key

--- roles/create_asg/files/user_data.sh
+++ roles/create_asg/files/user_data.sh
@@ -6,3 +6,4 @@ sudo usermod -a -G docker ec2-user
 sudo service docker start
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/amila-ku/wordpress-prometheus.git wp
