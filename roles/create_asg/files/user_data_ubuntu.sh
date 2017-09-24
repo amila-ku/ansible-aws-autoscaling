b9e789e949d2c0e6cb10a8b5571a879dd65ad395
Amila Kumaranayaka <amila.kumaranayaka@cloudpac.io>
2017-09-24 16:50:21 +0530

    changing AMI to ubuntu due to cadvisor issue with amazon linux

---
+++ roles/create_asg/files/user_data_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce git
+sudo usermod -a -G docker ubuntu
+sudo service docker start
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo git clone https://github.com/amila-ku/wordpress-prometheus.git /wp
+cd /wp
+docker-compose up -d
